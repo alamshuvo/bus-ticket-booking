b17397b2bd34c2f2b851b8b8a51bf2b049b5f207
iftakhar <iftakharalamshuvo11@gmail.com>
2024-02-18 23:13:44 +0600

    added

--- script.js
+++ script.js
@@ -109,27 +109,3 @@ btn.addEventListener("click",function () {
 
 
 
-
-
-
-// some common shared function
-// get value an html element
-function getValueForElement(id) {
-    const getElement = document.getElementById(id).innerText;
-    const convertedValue=parseInt(getElement);
-    return convertedValue;
-}
-// set inner text
-function setInnerText(id,value) {
-    const getElement=document.getElementById(id).innerText=value;
-}
-// add class list
-function addClassList(id,value) {
-    const getElement=document.getElementById(id);
-    getElement.classList.add(value);
-}
-// remove class list
-function removeClassList(id,value) {
-    const getElement=document.getElementById(id);
-    getElement.classList.remove(value);
-}
